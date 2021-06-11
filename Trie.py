851f7bad12edb31cf72a201b0b8c2fd0fce8bbc7
Tadedp <83606020+Tadedp@users.noreply.github.com>
2021-06-11 13:19:27 -0300

    Trie 5.0
    Adaptado al idioma inglés y debuggeado.

--- Trie.py
+++ Trie.py
@@ -26,7 +26,7 @@ def insert(T,element):
   #Se valida la entrada.
   if element == "" :
     return None
-  
+
   element = validateInput(element)
   if element == None or element == "":
     return None
@@ -35,11 +35,11 @@ def insert(T,element):
   #Se verifica si el árbol se encuentra vacío.
   if T.root == None:
     T.root = TrieNode()
-    ASCIIArray = Array(74,ArrayNode())
+    ASCIIArray = Array(62,ArrayNode())
     T.root.children = ASCIIArray
   else:
     ASCIIArray = T.root.children
-    
+
 
   #ASCIIArray = T.root.children en teoria esta de mas, cuidado variable apuntador
 
@@ -51,7 +51,7 @@ def insert(T,element):
     if length - 1 != i:
       if i == 0: 
         if ASCIIArray[position].children == None:
-          ASCIIArray[position].children = Array(74,ArrayNode())
+          ASCIIArray[position].children = Array(62,ArrayNode())
       else:
         if ASCIIArray[position].children == None:
           ASCIIArray[position].children = LinkedList()
@@ -59,8 +59,8 @@ def insert(T,element):
       ASCIIArray[position].isEndOfWord = ASCIIArray[position].isEndOfWord + 1
       return element
     if i != 1:
-     ASCIIArray = ASCIIArray[position].children
-             
+      ASCIIArray = ASCIIArray[position].children
+
 
   currentNode = ASCIIArray[position]
   #Se crea un bucle para insertar cada caracter del elemento en un nodo nuevo, recorriendo nivel por nivel del árbol.
@@ -90,10 +90,10 @@ def insert(T,element):
       #Si no tiene hijos, como children del nodo padre se crea una LinkedList y en su value se insertará el nodo con key igual al caracter correspondiente de la string.
       currentNode.children = LinkedList()
       currentNode.children.head = newNode
-      
+
       #Se actualizan los nodos para recorrer el árbol.
       currentNode = currentNode.children.head
-      
+
     #Si el nodo insertado tenía al último caracter de la string, su isEndOfWord se actualiza a true y se termina la ejecución del bucle, dejando a su children como None.
     if i == length - 1:
       currentNode.isEndOfWord = currentNode.isEndOfWord + 1
@@ -103,80 +103,37 @@ def insert(T,element):
 #Output: Devuelve un codigo 
 #Function: Modifica sobre la entrada para devolver un codigo de salida a corde a la posicion del caracter dentro del arreglo ASCII
 def hashAlphabet(key):
-    if key == None:return None
-    key = ord(key)
-    #Letras de la "a" a la "z" con ñ incluida
-    if (key <= 122 and key >= 97) or key == 241: 
-        if key == 241:
-            key = 52
-        else:
-            key = key - 71
-        return key
-    #Letras de la "A" a las "Z" con Ñ incluida
-    if (key <= 90 and key >= 65) or key == 209: 
-        if key == 209:
-            key = 53
-        else:
-            key = key - 65
-        return key
-    #Vocales con tilde, mayusculas y minusculas.
-    if key == 225:#"á"
-        key = 64
-        return key
-    if key == 233:#"é"
-        key = 65
-        return key
-    if key == 237:#"í"
-        key = 66
-        return key
-    if key == 243:#"ó"
-        key = 67  
-        return key  
-    if key == 250:#"ú"
-        key = 68
-        return key
-    if key == 193:#"Á"
-        key = 69
-        return key
-    if key == 201:#"É"
-        key = 70
-        return key
-    if key == 205:#"Í"
-        key = 71
-        return key
-    if key == 211:#"Ó"
-        key = 72 
-        return key
-    if key == 218:#"Ú"
-        key = 73 
-        return key
-    #Numeros del 0 al 9.
-    if key <= 57 and key >= 48:
-        key = key + 6
-        return key
+  if key == None:
+    return None
+  key = ord(key)
+  #Letras de la "a" a la "z"
+  if key <= 122 and key >= 97: 
+    key = key - 71
+    return key
+  #Letras de la "A" a las "Z"
+  if key <= 90 and key >= 65: 
+    key = key - 65
+    return key
+  #Numeros del 0 al 9.
+  if key <= 57 and key >= 48:
+      key = key + 4
+      return key
 
 #Input: Recibe un string 
 #Output: Nuevo string depurado
 #Function: Revisa letra por letra el string, eliminando todo caracter no permitido y devuelve un string nuevo sin estos caracteres.
 def validateInput(string):
-    if string == None:
-        return None
-    else:
-        length = len(string)
-        auxString = ""
-        for x in range(0, length):
-            key = ord(string[x])
-
-            #Letras de la "a" a la "z" con ñ incluida
-            if (key <= 122 and key >= 97) or key == 241: 
-                auxString = auxString + string[x]
-            #Letras de la "A" a las "Z" con Ñ incluida
-            if (key <= 90 and key >= 65) or key == 209: 
-                auxString = auxString + string[x]
-            #Vocales con tilde, mayusculas y minusculas.
-            if key == 225 or key == 233 or key == 237 or key == 243 or key == 250 or key == 193 or key == 201 or key == 205 or key == 211 or key == 218:
-                auxString = auxString + string[x]
-            #Numeros del 0 al 9.
-            if key <= 57 and key >= 48:
-                auxString = auxString + string[x]
-        return auxString
+  length = len(string)
+  auxString = ""
+  for x in range(0, length):
+    key = ord(string[x])
+    #Letras de la "a" a la "z"
+    if key <= 122 and key >= 97: 
+      auxString = auxString + string[x]
+    #Letras de la "A" a las "Z"
+    if key <= 90 and key >= 65: 
+      auxString = auxString + string[x]
+    #Numeros del 0 al 9.
+    if key <= 57 and key >= 48:
+      auxString = auxString + string[x]
+  return auxString 
