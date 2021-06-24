dab13f6c58ee3541370ae48fee975899fc9c489a
Facutomba122 <facundoelst@gmail.com>
2021-06-24 19:32:25 -0300

    Creacion de la biblioteca 1.0
    Necesita terminar la funcion saveLibrary que se encarga de guardar la libreria en disco duro.

---
+++ create_library.py
@@ -0,0 +1,30 @@
+from Trie import *
+from algo1 import *
+import os
+
+#Input: recibe una direccion de ficheros
+#Output: devuelve un arreglo con la libreria de Trie almacenados
+#Funcion: Dado un directorio, la funcion createLibrary crea un arreglo de n elementos, para los cuales creara un Trie por cada elemento.
+def createLibrary(localPath):
+    directory = os.listdir(localPath)
+    library = Array(len(directory), Array(2, None))
+    for x in range(0, len(directory)):
+        trie = Trie()
+        currentTxt = open(localPath+ "/" + directory[x], encoding='utf-8')
+        currentLine = currentTxt.read()
+        length = len(currentLine)
+        auxString = ""
+        for n in range(0, length):
+            if currentLine[n] != " " and currentLine[n] != "." and currentLine[n] != "," and currentLine[n] != "?" and currentLine[n] != "!" and currentLine[n] != "\n":
+                auxString = auxString + currentLine[n]
+            else:
+                insert(trie, auxString)
+                auxString = ""
+        library.data[x].data[0] = directory[x]
+        library.data[x].data[1] = trie
+    currentTxt.close
+    saveLibrary(library)
+    return library
+
+def saveLibrary(library):
+    
--- personal_library.py
+++ personal_library.py
@@ -1,21 +1,20 @@
-from Trie import *
-import os
-from algo1 import *
-localPath = os.listdir("C:\DataTest")
-library = Array(len(localPath), Array(2, None))
-print(len(localPath))
-for x in range(0, len(localPath)):
-    trie = Trie()
-    currentTxt = open("C:\DataTest/" + localPath[x], encoding='utf-8')
-    currentLine = currentTxt.read()
-    length = len(currentLine)
-    auxString = ""
-    for n in range(0, length):
-        if currentLine[n] != " " and currentLine[n] != "." and currentLine[n] != "," and currentLine[n] != "?" and currentLine[n] != "!" and currentLine[n] != "\n":
-            auxString = auxString + currentLine[n]
+import create_library
+import sys
+
+def main(argv, key):
+    if key == 0: #comando create
+        create_library.createLibrary(argv)
+    else:
+        #Llamado al algoritmo de busqueda al estilo, search.search(argv)
+        print("Brandom Sanderson")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3: print("invalid Input")
+    else:
+        if sys.argv[1] == "-create":
+            main(sys.argv[2], 0)
+        elif sys.argv[1] == "-search":
+            main(sys.argv[2], 1)
         else:
-            insert(trie, auxString)
-            auxString = ""
-    library.data[x].data[0] = localPath[x]
-    library.data[x].data[1] = trie
-currentTxt.close
+            print("unknown command")
